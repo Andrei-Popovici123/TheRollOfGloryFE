abbea9fd6707a56382e7caedaa1d8df9d9ad2f50
Andrei Popovici <andrei-cristian.popovici@student.upt.ro>
2025-01-16 15:05:33 +0100

    application finally deployed to firebase and operational

--- src/app/pages/registration-aux/login-page/login-page.component.ts
+++ src/app/pages/registration-aux/login-page/login-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {SidebarComponent} from '../../../common/sidebar/sidebar.component';
-import {Auth,signInWithPopup,GoogleAuthProvider} from '@angular/fire/auth';
+// import {Auth,signInWithPopup,GoogleAuthProvider} from '@angular/fire/auth';
 
 
 
@@ -17,18 +17,18 @@ import {Auth,signInWithPopup,GoogleAuthProvider} from '@angular/fire/auth';
 export class LoginPageComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder,private auth: Auth) {
+  constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
-  login() {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(this.auth, provider)
-      .then(result => console.log('User signed in:', result.user))
-      .catch(err => console.error('Error signing in:', err));
-  }
+  // login() {
+  //   const provider = new GoogleAuthProvider();
+  //   signInWithPopup(this.auth, provider)
+  //     .then(result => console.log('User signed in:', result.user))
+  //     .catch(err => console.error('Error signing in:', err));
+  // }
   submitLogin(): void {
     if (this.loginForm.valid) {
       const formData = { ...this.loginForm.value };
